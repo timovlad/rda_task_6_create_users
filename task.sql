b6b8a66dab32073c6ca6379ad4803692ae83d333
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-10 21:47:09 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,1 +1,9 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
